42c8b5102eaa5d416eef1959695b195a1b4aa090
Blaine R Love <blainelove94@gmail.com>
2021-01-03 19:08:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,7 @@ def long_planeteer_calls(planeteer_calls)
       return true
     end
   end
+end
 
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
