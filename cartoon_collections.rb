946107711102269a924dbec473c29de2085505ae
Blaine R Love <blainelove94@gmail.com>
2021-01-03 19:07:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,10 +22,10 @@ end
 
 
 def long_planeteer_calls(planeteer_calls)
-
   planeteer_calls.each do |element|
     if element =< 4
-      return true 
+      return true
+    end
 
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
