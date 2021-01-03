da3ca82eba17355b973c365600b98b8f0b024dda
Blaine R Love <blainelove94@gmail.com>
2021-01-03 19:08:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,7 +31,7 @@ end
 
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-end
+
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
