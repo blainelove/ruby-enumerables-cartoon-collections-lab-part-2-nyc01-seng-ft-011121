c1b22e57dd0651635c1bfd8f1618914b0c5ff332
Blaine R Love <blainelove94@gmail.com>
2021-01-03 19:33:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -41,6 +41,7 @@ def find_valid_calls(planeteer_calls)
       return element
     end
   end
+  return nil
 
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
