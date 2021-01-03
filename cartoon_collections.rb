dcba0de45a3f37103ff52103e849d51cdc160b18
Blaine R Love <blainelove94@gmail.com>
2021-01-03 19:49:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -38,7 +38,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.each do |element|
     if valid_calls.include? element
-      return true
+      return element
     end
   end
   return nil
